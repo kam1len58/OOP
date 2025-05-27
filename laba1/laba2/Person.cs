dfb24b832545343520ed1610cbc1e20d862f2275
Kamil <kamil.228.garipov@gmal.com>
2025-05-27 13:11:32 +0300

    laba2 completed

---
+++ laba1/laba2/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace laba2;
+
+public class Person
+{
+    public string Name { get; set; }
+
+    public Person(string name)
+    {
+        Name = name;
+    }
+    ~Person()
+    {
+        Name = "";
+    }
+
+    public override string ToString()
+    {
+        return $"Hello! My name is {Name}";
+    }
+}
---
+++ laba1/laba2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace laba2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person[] people = new Person[3];
+        for(int i = 0; i < people.Length; i++)
+        {
+            string name = Console.ReadLine();
+            people[i]=new Person(name);
+        }
+        foreach(var person in people)
+        {
+            Console.WriteLine(person.ToString());
+        }
+    }
+}
