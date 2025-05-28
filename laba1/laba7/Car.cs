2f29951e26903b646ccfca0f8bba477fbb452a81
Kamil <kamil.228.garipov@gmal.com>
2025-05-28 21:15:36 +0300

    laba7 completed

---
+++ laba1/laba7/Car.cs
@@ -0,0 +1,25 @@
+﻿namespace laba7;
+
+class Car : IVehiculo
+{
+    public int FuelLevel { get; set; }
+
+    public Car(int fuelLevel)
+    {
+        FuelLevel = fuelLevel;
+    }
+
+    public void Drive()
+    {
+        if (FuelLevel > 0)
+        {
+            Console.WriteLine("Driving");
+        }
+    }
+
+    public bool Refuel(int fuelLevel)
+    {
+        FuelLevel++;
+        return true;
+    }
+}
---
+++ laba1/laba7/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace laba7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car(0);
+        int fuelLevel;
+        do
+        {
+            Console.WriteLine("Введите кол-во бензина: ");
+            fuelLevel = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+        }
+        while (fuelLevel == 0);
+
+        bool fuelAmount = car.Refuel(fuelLevel);
+
+        car.Drive();
+    }
+}
