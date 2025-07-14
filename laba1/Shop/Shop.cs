3ce4282ab1a805dc22bbac8eb3110e57dd1db1af
Kamil <kamil.228.garipov@gmal.com>
2025-07-14 20:02:01 +0300

    1-3 items work Shop completed

---
+++ laba1/Shop/Shop.cs
@@ -0,0 +1,23 @@
+﻿namespace Shop;
+
+class Shop
+{
+    public int Code { get; }
+    public string Name { get; }
+    public string Address { get; }
+
+    public Shop(int code, string name, string address)
+    {
+        Code = code;
+        Name = name;
+        Address = address;
+    }
+
+    List<(Product product, int quanity, int price)> productSet = new();
+
+    public void DeliveryOfGoods(Product product, int quanity, int price)
+    { 
+        productSet.Add((product, quanity, price));
+    } 
+    
+}
