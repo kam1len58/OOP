81d84e37767e93e20a26be87f7a96f94a89ecdba
kam1len58 <kamil.228.garipov@gmail.com>
2025-07-25 21:54:01 +0300

    1-3 items work Shop completed (#10)
    * 1-3 items work Shop completed
    
    * fix bugs
    
    * bug fixed
    
    * tests method
    
    * Added test-Comparison of actual and expected batches and fix bugs
    
    * refactor: Changing the List collection to a Dictionary
    
    ---------
    
    Co-authored-by: Kamil <kamil.228.garipov@gmal.com>

---
+++ laba1/Shop/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace WorkShop;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Shop shop1 = new Shop(1, "Магнит", "пр. Мира, 20");
+        Shop shop2 = new Shop(2, "Пятерочка", "ул. Ершова, 50");
+        Shop shop3 = new Shop(3, "Перекрёсток", "ул. Авангардная, 40");
+
+        shop1.DeliveryBatchProducts
+        (
+            (new Product(1, "ХЛЕБ"), 80, 45),
+            (new Product(2, "МОЛОКО"), 30, 85),
+            (new Product(3, "РИС"), 40, 120),
+            (new Product(4, "МАСЛО"), 25, 150),
+            (new Product(5, "МЯСО"), 15, 400),
+            (new Product(6, "РЫБА"), 20, 300),
+            (new Product(7, "ЯЙЦА"), 100, 90),
+            (new Product(8, "САХАР"), 60, 60),
+            (new Product(9, "СОЛЬ"), 50, 30),
+            (new Product(10, "КАРТОФЕЛЬ"), 200, 25),
+            (new Product(10, "КАРТОФЕЛЬ"), 200, 25)
+        );
+    }
+}
