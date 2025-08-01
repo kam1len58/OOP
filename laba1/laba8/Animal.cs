f1e5e67776603cb623cf6711b60922a785d4fa2e
kam1len58 <kamil.228.garipov@gmail.com>
2025-08-01 22:00:34 +0300

    laba8 completed (#8)
    * laba8 completed
    
    * fix: set and get name in work 8
    
    ---------
    
    Co-authored-by: Kamil <kamil.228.garipov@gmal.com>

---
+++ laba1/laba8/Animal.cs
@@ -0,0 +1,8 @@
+﻿namespace WorkEight;
+
+abstract class Animal
+{
+    public required string Name { get; init; }
+
+    public abstract void Eat();
+}
---
+++ laba1/laba8/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace WorkEight;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog dog;
+        string? name;
+        do
+        {
+            name = Console.ReadLine()!;
+            dog = new Dog { Name = name };
+            Console.Clear();
+        }
+        while (name == null || name.Length == 0);
+        Console.WriteLine(dog.Name);
+        dog.Eat();
+    }
+}
